e3e4c93a0758710bbd057c72e197621beb473ddb
musse <mus.sedat.aksoy@gmail.com>
2024-01-09 22:16:09 +0300

    Cucumber Framework hazırlık çalışmalarım 1 Gherkin dili ile test senaryosu oluşturma.

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,28 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+
+public class _01_LoginSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+
+
+
+    }
+
+    @When("Enter username and password and click login button")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+
+
+
+    }
+
+    @Then("User should login successfuly")
+    public void userShouldLoginSuccessfuly() {
+
+
+
+
+    }
+}
